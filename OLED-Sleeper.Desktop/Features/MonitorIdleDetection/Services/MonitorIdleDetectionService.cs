61f4fd43a304119b8ddeb1c560dce20eae543b52
Derek Gooding <djgooding@live.com>
2026-03-19 10:08:27 -0400

    missed a using

--- OLED-Sleeper.Desktop/Features/MonitorIdleDetection/Services/MonitorIdleDetectionService.cs
+++ OLED-Sleeper.Desktop/Features/MonitorIdleDetection/Services/MonitorIdleDetectionService.cs
@@ -5,7 +5,6 @@ using OLED_Sleeper.Features.MonitorIdleDetection.Services.Interfaces;
 using OLED_Sleeper.Features.MonitorInformation.Models;
 using OLED_Sleeper.Features.MonitorInformation.Services.Interfaces;
 using OLED_Sleeper.Features.UserSettings.Models;
-using OLED_Sleeper.Native;
 using Serilog;
 using System.Runtime.InteropServices;
 
